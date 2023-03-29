fb5b4515af7f19ddae2e70fd08b77ec73031625b
Zehan Khan <zehan9211@gmail.com>
2023-03-29 17:01:26 +0530

    express added and created a basic server

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+const express = require("express");
+const app = express()
+const port = 4200;
 
+app.get("/", (req, res) => {
+    res.send("hello from backend")
+})
 
-
+app.listen(port, () => {
+    console.log(`Server listing on port ${port}`)
+})
