8afadc985bdf62e3ad60708ceb24a3ea43dc245c
Zehan Khan <zehan9211@gmail.com>
2023-03-29 17:57:41 +0530

    update data

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const products = require("./mock.json")
+
 const app = express()
 const port = 4200;
 
 app.get("/", (req, res) => {
     res.send("hello from backend")
+})
+
+app.get("/products",(req, res)=>{
+    res.status(200).json(products)
 })
 
 app.listen(port, () => {
