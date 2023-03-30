1790b0c23094391f1988b96222df989150f92d40
Zehan Khan <zehan9211@gmail.com>
2023-03-30 17:41:06 +0530

    tailwind setup

--- client/src/container/Main.jsx
+++ client/src/container/Main.jsx
@@ -2,7 +2,7 @@
 const Main = () => {
   return (
     <main>
-      <h1>App render</h1>
+      <h1 className="text-center font-mono text-6xl font-semibold py-20 bg-purple-400 text-red-800">App render</h1>
     </main>
   )
 }
