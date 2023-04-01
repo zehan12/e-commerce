321daec0949b74583005b05f9fe9fe9c924820d2
Zehan Khan <zehan9211@gmail.com>
2023-04-01 15:45:49 +0530

    :boom: add gradient and added react-useAnmimation"

--- client/src/components/landing/Parallax.jsx
+++ client/src/components/landing/Parallax.jsx
@@ -11,4 +11,4 @@ const Parallax = () => {
     )
 }
 
-return Parallax;
+export default Parallax;
--- client/src/page/screen/Landing.jsx
+++ client/src/page/screen/Landing.jsx
@@ -1,8 +1,12 @@
 import { Fragment } from "react";
+import UseAnimations from "react-useanimations";
+import github from 'react-useanimations/lib/github';
+
 import Content from "../../components/landing/Content";
 import Parallax from "../../components/landing/Parallax";
 import VideoBackground from "../../components/landing/VideoBackground";
 import Header from "../../components/layout/Header";
+
 
 const Landing = () => {
     return (
@@ -11,7 +15,15 @@ const Landing = () => {
             <Content />
             <VideoBackground />
             <Parallax />
-            <div className="h-[30vh] bg-[crimson]"></div>
+            <div className="h-[30vh] bg-[rgb(255,163,255)]" style={{ background: "linear-gradient(0deg, rgba(255,163,255,1) 0%, rgba(255,211,78,1) 100%)" }} >
+                <a
+                    className="flex justify-center items-center p-20"
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    href='https://github.com/zehan12'>
+                    <UseAnimations animation={github} size={90} />
+                </a>
+            </div>
         </Fragment>
     )
 }
