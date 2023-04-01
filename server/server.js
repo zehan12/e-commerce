86b43cdf9ed1dfc5783703e0160dfb34707d689c
Zehan Khan <zehan9211@gmail.com>
2023-04-01 19:56:09 +0530

    :loud_sound: add chalks logs

--- server/server.js
+++ server/server.js
@@ -3,11 +3,11 @@ require("dotenv").config();
 const express   = require("express");
 const products  = require("../mock.json")
 const mongoose  = require("mongoose");
-const chalk=require("chalk");
+const chalk     = require("chalk");
 
-const app       = express()
-const port      = process.env.PORT || 4200;
-const connUri   = process.env.MONGO_URI;
+const app = express()
+const port = process.env.PORT || 4200;
+const connUri = process.env.MONGO_URI;
 
 //=== - SET UP DATABASE
 //Configure mongoose's promise to global promise
@@ -16,9 +16,9 @@ mongoose.promise = global.Promise;
 mongoose.connect(connUri);
 
 const connection = mongoose.connection;
-connection.once('open', () => console.log('MongoDB --  database connection established successfully!'));
+connection.once('open', () => console.log(chalk.bgGreen.white.bold("MongoDB") + " -- " + chalk.blue.underline.bold("database connection established successfully!")));
 connection.on('error', (err) => {
-    console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
+    console.log(chalk.green.bold("MongoDB") + "  " + chalk.blue.underline.bold("connection error. Please make sure MongoDB is running. " + err));
     process.exit();
 });
 
@@ -26,9 +26,9 @@ connection.on('error', (err) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/v1.0/user",require("./routes/user"));
+app.use("/api/v1.0/user", require("./routes/user"));
 app.use("/api/v1.0/register", require("./routes/register"));
-app.use("/api/v1.0/authenticate",require("./routes/authenticate"));
+app.use("/api/v1.0/authenticate", require("./routes/authenticate"));
 
 app.get("/", (req, res) => {
     res.send("hello from backend")
@@ -39,5 +39,5 @@ app.get("/products", (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(chalk.red.bold(`Server listing on port ${port}`))
+    console.log(chalk.red.bold("Server listing on port") + ": " + chalk.blue.underline.bold(port))
 })
