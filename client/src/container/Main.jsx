4037794456b6c8d89e888de49feb2889c4e9c64d
Zehan Khan <zehan9211@gmail.com>
2023-04-12 09:03:16 +0530

    test: :wrench: create app for  test

--- client/src/container/Main.jsx
+++ client/src/container/Main.jsx
@@ -1,10 +1,12 @@
 import 'animate.css';
 import Landing from "../page/screen/Landing";
+import App from '../page/screen/App';
 
 const Main = () => {
   return (
     <main>
-      <Landing />
+      {/* <Landing /> */}
+      <App />
     </main>
   )
 }
---
+++ client/src/page/screen/App.jsx
@@ -0,0 +1,11 @@
+import { Fragment } from "react"
+
+const App = () => {
+    return(
+        <Fragment>
+            <h1>App</h1>
+        </Fragment>
+    )
+}
+
+export default App;
