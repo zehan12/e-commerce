d3427ca82f8f1067fdac217443d9f4e8aa9e19ec
Zehan Khan <zkhan@hootboard.com>
2023-04-16 00:02:39 +0530

    fix: :white_check_mark: app and server

--- server/server.js
+++ server/server.js
@@ -1,74 +1,48 @@
-require("dotenv").config();
-
-// external lib
-const express = require("express");
-const products = require("../mock.json")
-const mongoose = require("mongoose");
-const chalk = require("chalk");
-
 // internal lib
 const cluster = require("cluster");
 const os = require("os");
-const { fork } = require("child_process");
 
-const app = express()
-const port = process.env.PORT || 4200;
-const connUri = process.env.MONGO_URI;
+// internal imports
+const app = require("./app");
+const config = require("./config/config");
+const connectDB = require("./config/db");
+
+const serverPort = config.port;
 
 // get no. of port present in our cup
 const noOfCpus = os.cpus().length;
 
-//=== - SET UP DATABASE
-//Configure mongoose's promise to global promise
-mongoose.set('strictQuery', false)
-mongoose.promise = global.Promise;
-mongoose.connect(connUri);
+connectDB();
 
-const connection = mongoose.connection;
-connection.once('open', () => console.log(chalk.bgGreen.white.bold("MongoDB") + " -- " + chalk.blue.underline.bold("database connection established successfully!")));
-connection.on('error', (err) => {
-    console.log(chalk.green.bold("MongoDB") + "  " + chalk.blue.underline.bold("connection error. Please make sure MongoDB is running. " + err));
-    process.exit();
+// handle when EPIPE error comes up
+process.stdout.on('error', function (err) {
+    if (err.code == "EPIPE") {
+        process.exit(0);
+    }
 });
 
-// configuring middleware needed for authentication
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+if (config.env === "production") {
+    if (cluster.isMaster) {
+        console.log(process.pid + " In Master")
+        for (let i = 0; i < noOfCpus; i++) {
+            cluster.fork();
+        }
+        cluster.on('exit', () => {
+            console.log('one worker destroy');
+            cluster.fork()
+        })
+    } else {
+        app.listen(serverPort)
+    }
+} else {
+    app.listen(serverPort,
+        () => {
+            console.log(`
+            ################################################
+            🚀 Server listening on port: ${serverPort} 🚀
+            ################################################
+    `)
+        })
+}
 
-app.use("/api/v1.0/user", require("./routes/user"));
-app.use("/api/v1.0/register", require("./routes/register"));
-app.use("/api/v1.0/authenticate", require("./routes/authenticate"));
 
-app.get("/api/dir", (_, res) => {
-    const childProcess = fork("./server/utils/dir")
-    childProcess.send({ "dir": process.cwd() });
-    childProcess.on("message", message => res.send(message));
-})
-
-app.get("/products", (_, res) => {
-    res.status(200).json(products)
-})
-
-app.get("/", (_, res) => {
-    res.send("Hello from Backend")
-})
-
-// handle unknown routes
-app.get("*", (_, res) => res.status(404).send("Page not found"));
-
-// if (cluster.isMaster) {
-//     console.log(process.pid + " In Master")
-//     for (let i = 0; i < noOfCpus; i++) {
-//         cluster.fork();
-//     }
-//     cluster.on('exit', () => {
-//         console.log('one worker destroy');
-//         cluster.fork()
-//     })
-// } else {
-    // app.listen(port, () => {
-    //     console.log(chalk.red.bold(`Server ${process.pid} listing on port`) + ": " + chalk.blue.underline.bold(port) + ` worker: ${cluster.worker.id}`)
-    // })
-// }
-
-app.listen(port,()=>{console.log("port is running")})
