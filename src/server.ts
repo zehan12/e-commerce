304aacd795ba219d85e9652b38b43aad07ad25e0
zehan khan <zehan9211@gmail.com>
2024-05-07 13:10:39 +0530

    migrate to typescript

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
-const server = require("./app");
+import app from "./app";
 const PORT = 3000;
 
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`[server]: Server is running at http://localhost:${PORT}`);
 });
