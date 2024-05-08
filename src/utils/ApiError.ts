3d099039bdf8087c17c5688e16cc460026abdc42
zehan khan <zehan9211@gmail.com>
2024-05-08 17:46:28 +0530

    add api error handler

---
+++ src/utils/ApiError.ts
@@ -0,0 +1,9 @@
+class ApiError extends Error {
+    statusCode: number;
+    constructor(message: string, statusCode: number) {
+        super(message);
+        this.statusCode = statusCode
+
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
